7d24bf06488948cc18de2ba86fe2aced6271c4fb
Daniel Romero <daniel.romero@lurica.com.ar>
2025-05-15 15:16:45 -0300

    view server

--- app/model/web.php
+++ app/model/web.php
@@ -9,6 +9,7 @@ use AFIP\Exceptions\AfipLoginException;
  
 $App->get('index', function ()
 {
+    var_dump($_SERVER);
     $this->data->set("rand",rand(111,999) );
     $this->parser->parse(BASEPATH."ui/index.html", $this->data->get());
 });
